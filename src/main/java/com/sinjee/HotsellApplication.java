c00a27727b6bafb8df085e61d8ee940f3b13ba33
kweitan <chen_guixin@163.com>
2019-12-12 23:26:15 +0800

    Initial commit

---
+++ src/main/java/com/sinjee/HotsellApplication.java
@@ -0,0 +1,13 @@
+package com.sinjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotsellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotsellApplication.class, args);
+    }
+
+}

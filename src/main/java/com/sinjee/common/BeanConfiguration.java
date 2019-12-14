3b3d28c67e3c0419bef2ff7fcfba93e0f1767647
kweitan <chen_guixin@163.com>
2019-12-14 16:24:05 +0800

    架构

---
+++ src/main/java/com/sinjee/common/BeanConfiguration.java
@@ -0,0 +1,20 @@
+package com.sinjee.common;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author 小小极客
+ * 时间 2019/12/14 12:54
+ * @ClassName BeanConfiguration
+ * 描述 配制自定义Bean
+ **/
+@Configurable
+public class BeanConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

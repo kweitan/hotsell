849aee500583b12815cce27761e5cf2ffea08461
kweitan <43361375+kweitan@users.noreply.github.com>
2020-02-02 22:47:18 +0800

    更新

--- src/main/java/com/sinjee/wechat/utils/WechatAccessTokenUtil.java
+++ src/main/java/com/sinjee/wechat/utils/WechatAccessTokenUtil.java
@@ -1,5 +1,9 @@
 package com.sinjee.wechat.utils;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.sinjee.wechat.dto.BuyerInfoDTO;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -19,13 +23,18 @@ public class WechatAccessTokenUtil {
     @Value("${wechat.accessToekenScret}")
     private static String tokenSecret ;
 
+    /**
+     * 签名生成
+     * @param user
+     * @return
+     */
     public static String sign(BuyerInfoDTO user){
         String token = null;
         try {
             Date expiresAt = new Date(System.currentTimeMillis() + expireTime);
             token = JWT.create()
                     .withIssuer("auth0")
-                    .withClaim("username", user.getUsername())
+                    .withClaim("openid", user.getOpenId())
                     .withExpiresAt(expiresAt)
                     // 使用了HMAC256加密算法。
                     .sign(Algorithm.HMAC256(tokenSecret));
@@ -33,5 +42,22 @@ public class WechatAccessTokenUtil {
             e.printStackTrace();
         }
         return token;
+    }
+
+    public static boolean verify(String token){
+        try {
+            JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(tokenSecret)).withIssuer("auth0").build();
+            DecodedJWT jwt = jwtVerifier.verify(token) ;
+            //1.取出issuer
+            jwt.getIssuer();
+            //2.取出openid
+            jwt.getClaim("openid");
+            //3.取出过期时间
+            jwt.getExpiresAt();
+        }catch (Exception e){
+
+        }
+
+        return true;
     }
 }
